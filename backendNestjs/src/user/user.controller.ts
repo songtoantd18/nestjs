1741ec75775c729c227c52e2762c4886c6ba4695
MSI\PC <songtoantd18@gmail.com>
2024-12-19 10:41:34 +0700

    add guard

--- backendNestjs/src/user/user.controller.ts
+++ backendNestjs/src/user/user.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post,Put, Body, Delete, Param, BadRequestException } from '@nestjs/common';
+import { Controller, Get, Post,Put, Body, Delete, Param, BadRequestException, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './schemas/user.schema';
 import { isValidObjectId } from 'mongoose';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('users')
 export class UserController {
@@ -9,6 +10,7 @@ export class UserController {
 
   // Lấy danh sách tất cả users
   @Get()
+  @UseGuards(AuthGuard)
   async getAll() {
     console.log('Step 1: Nhận request tại Controller - /users');
     return this.userService.getAllUsers();
@@ -17,17 +19,21 @@ export class UserController {
   // Tạo mới user
   @Post()
   async create(@Body() user: User) {
+    console.log("🚀 ~ UserController ~ create ~ user: đây là tạo create new user", user)
     return this.userService.createUser(user);
   }
 
   // Lấy user theo ID
   @Get(':id')
   async getById(@Param('id') id: string) {
+    console.log("🚀 ~ UserController ~ getById ~ id: get user by id", id)
     return this.userService.getUserById(id);
   }
 // Cập nhật thông tin user theo ID
   @Put(':id')
 async update(@Param('id') id: string, @Body() user: User) {
+  console.log("🚀 ~ UserController ~ update ~ user: change data", user)
+  console.log("🚀 ~ UserController ~ update ~ id:", id)
   if (!isValidObjectId(id)) {
     throw new BadRequestException('ID không hợp lệ');
   }
@@ -36,6 +42,7 @@ async update(@Param('id') id: string, @Body() user: User) {
   // Xóa user theo ID
   @Delete('delete')
   async delete(@Body() body: { id: string }) {
+    console.log("🚀 ~ UserController ~ delete ~ id: delete dữ liệu", body.id)
     if (!isValidObjectId(body.id)) {
       throw new BadRequestException('ID không hợp lệ');
     }

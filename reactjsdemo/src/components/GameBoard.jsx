3c5dcf874bcc4a8fbd9445a82e6917f0fc999fe0
MSI\PC <songtoantd18@users.noreply.github.com>
2025-05-19 13:53:41 +0700

    thêm nút tích khi click vào

--- reactjsdemo/src/components/GameBoard.jsx
+++ reactjsdemo/src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 const initialTable = [
   [null, null, null],
   [null, null, null],
@@ -5,19 +7,44 @@ const initialTable = [
 ];
 
 export default function GameBoard() {
-    console.log("🚀 ~ GameBoard ~ initialTable:", initialTable);
-  return (
+  const [valueChosen, setValueChosen] = useState(initialTable);
+  //   console.log("🚀 ~ GameBoard ~ initialTable:", initialTable);
+  function handleSelected(rowIndex, colIndex) {
+    // initialTable[rowIndex][colIndex] = "X";
+    setValueChosen((prev) => {
+      console.log("🚀 ~ setValueChosen ~ prev:", prev);
+      const updateBoard = prev.map((innerArray) => [...innerArray]); // ✔ Gọn, đúng, đủ
+      console.log("🚀 ~ setValueChosen ~ updateBoard:", updateBoard);
 
+      updateBoard[rowIndex][colIndex] = "X";
+      return updateBoard;
+    });
+    // console.log("🚀 ~ handleSelected ~ initialTable:", initialTable);
+  }
+  return (
     <ol id="game-board">
-      {initialTable.map((row, rowIndex) => (
-        <li key={rowIndex} className="board-row">
-          {row.map((playerSymbol, colIndex) => (
-            <button key={colIndex} className="board-cell">
-              {playerSymbol}
-            </button>
-          ))}
-        </li>
-      ))}
+      {valueChosen.map((row, rowIndex) => {
+        // console.log("🚀 ~ GameBoard ~ row:", row);
+        // console.log("🚀 ~ GameBoard ~ rowIndex:", rowIndex);
+
+        return (
+          <li key={rowIndex} className="board-row">
+            {row.map((playerSymbol, colIndex) => {
+              //   console.log("🚀 ~ GameBoard ~ colIndex:", colIndex);
+              //   console.log("🚀 ~ GameBoard ~ playerSymbol:", playerSymbol);
+              return (
+                <button
+                  onClick={() => handleSelected(rowIndex, colIndex)}
+                  key={colIndex}
+                  className="board-cell"
+                >
+                  {playerSymbol}
+                </button>
+              );
+            })}
+          </li>
+        );
+      })}
     </ol>
   );
 }

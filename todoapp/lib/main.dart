242886a3cbe763cc6f4e91816cf85dd079f22dfd
MSI\PC <songtoantd18@users.noreply.github.com>
2025-06-12 14:26:52 +0700

    fix bug firebase

--- todoapp/lib/main.dart
+++ todoapp/lib/main.dart
@@ -2,25 +2,30 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'dart:math';
-import 'package:flutter/material.dart';
 import 'package:todoapp/category/create_or_edit_category.dart';
 import 'package:todoapp/main/main_page.dart';
 import 'package:todoapp/screen/login/login_page.dart';
 import 'package:todoapp/screen/onboarding/onboarding_page_view.dart';
 import 'package:todoapp/screen/welcome/welcome_page.dart';
-
 import 'screen/splashScreen.dart';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // ✅ Khởi tạo Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // ✅ Khởi tạo EasyLocalization
   await EasyLocalization.ensureInitialized();
+
   runApp(
     EasyLocalization(
       supportedLocales: const [Locale('vi'), Locale('en')],
       path: "assets/translations",
-
+      fallbackLocale: Locale('en'),
       child: MyApp(),
     ),
   );
@@ -28,6 +33,7 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -35,13 +41,7 @@ class MyApp extends StatelessWidget {
       localizationsDelegates: context.localizationDelegates,
       locale: context.locale,
       supportedLocales: context.supportedLocales,
-      // home: SplashScreen(),
-      // home: CreateOrEditCategory(),
-
-      // home: OnboardingPageView(),
-      // home: WelcomePage(),
-      // home: MainPage(),
-      home: LoginPage(),
+      home: LoginPage(), // Có thể đổi trang home ở đây
     );
   }
 }
